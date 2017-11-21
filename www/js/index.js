46292c281a6c8809fe67a4da2051cd2b8e841d8c
greyishmoon <chris@blythe.plus.com>
2017-11-21 19:54:42 +0000

    partial load fix

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,7 @@
 var map;
 
 function initMap() {
+
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: -34.397,
@@ -8,12 +9,18 @@ function initMap() {
         },
         zoom: 8
     });
+
+    setTimeout(function () {
+        google.maps.event.trigger(map, 'resize');
+    }, 500);
 }
+
+
 
 
 //when the jQuery Mobile page is initialised
 $(document).on('pageinit', onLoad);
 
 function onLoad() {
-    initMap();
+//    initMap();
 }
