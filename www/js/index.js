c068544b568e5cb77dea75217cd997a9082c62e4
greyishmoon <chris@blythe.plus.com>
2017-11-21 19:26:04 +0000

    working simple version

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,19 @@
-//when the jQuery Mobile page is initialised
-//$(document).on('pageinit', onLoad);
-//
-//function onLoad() {
-//    alert ("LOAD");
-//}
+var map;
 
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 8
+    });
+}
+
+
+//when the jQuery Mobile page is initialised
+$(document).on('pageinit', onLoad);
+
+function onLoad() {
+    initMap();
+}
