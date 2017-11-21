55d2e45f2b3d8e6be62441dbcfc0e89144b11092
greyishmoon <chris@blythe.plus.com>
2017-11-21 00:32:05 +0000

    3

--- www/js/index.js
+++ www/js/index.js
@@ -6,9 +6,11 @@
 //}
 
 var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
+
+// Called from googleapi hook
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+}
